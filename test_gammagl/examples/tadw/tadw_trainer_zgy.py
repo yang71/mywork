898756ae085336064fdf3afd4bc1d19400d2f7d1
BuptTab <gyzhou2000@gmail.com>
2023-06-19 14:40:14 +0800

    update

--- test_gammagl/examples/tadw/tadw_trainer_zgy.py
+++ test_gammagl/examples/tadw/tadw_trainer_zgy.py
@@ -10,19 +10,17 @@ import argparse
 import tensorlayerx as tlx
 from gammagl.datasets import Planetoid
 from gammagl.models import TADWModel
-from gammagl.utils import calc_gcn_norm, mask_to_index
-from tensorlayerx.model import TrainOneStep, WithLoss
-from sklearn.linear_model import LogisticRegression
+from sklearn import svm
+from sklearn import metrics
+from sklearn import model_selection
 
 
-def calculate_acc(train_z, train_y, test_z, test_y, solver='lbfgs', multi_class='auto', max_iter=150):
-    train_z = tlx.convert_to_numpy(train_z)
-    train_y = tlx.convert_to_numpy(train_y)
-    test_z = tlx.convert_to_numpy(test_z)
-    test_y = tlx.convert_to_numpy(test_y)
+def calculate_acc(train_z, train_y, test_z, test_y):
 
-    clf = LogisticRegression(solver=solver, multi_class=multi_class, max_iter=max_iter).fit(train_z, train_y)
-    return clf.score(test_z, test_y)
+    clf = svm.LinearSVC(C=5.0)
+    clf.fit(train_z, train_y)
+    predict_y = clf.predict(test_z)
+    return metrics.accuracy_score(test_y, predict_y)
 
 
 def main(args):
@@ -32,11 +30,6 @@ def main(args):
     dataset = Planetoid(args.dataset_path, args.dataset)
     graph = dataset[0]
     edge_index = graph.edge_index
-
-    # for mindspore, it should be passed into node indices
-    train_idx = mask_to_index(graph.train_mask)
-    test_idx = mask_to_index(graph.test_mask)
-    val_idx = mask_to_index(graph.val_mask)
 
     model = TADWModel(edge_index=edge_index,
                       embedding_dim=args.embedding_dim,
@@ -50,30 +43,32 @@ def main(args):
         "x": graph.x,
         "y": graph.y,
         "edge_index": graph.edge_index,
-        "train_idx": train_idx,
-        "test_idx": test_idx,
-        "val_idx": val_idx,
         "num_nodes": graph.num_nodes,
     }
 
+    best_test_acc = 0
+    z_test = 0
     for epoch in range(args.n_epoch):
         model.set_train()
         train_loss = model.fit()
         model.set_eval()
         z = model.campute()
 
-        val_acc = calculate_acc(tlx.gather(z, data['train_idx']), tlx.gather(graph.y, data['train_idx']),
-                                tlx.gather(z, data['val_idx']), tlx.gather(graph.y, data['val_idx']),
-                                max_iter=500)
+        train_x, test_x, train_y, test_y = model_selection.train_test_split(z, tlx.convert_to_numpy(data['y']),
+                                                                          test_size=0.2, shuffle=True)
+        test_acc = calculate_acc(train_x, train_y, test_x, test_y)
+        if test_acc > best_test_acc:
+            best_test_acc = test_acc
+            z_test = z
 
         print("Epoch [{:0>3d}] ".format(epoch + 1) \
-              + "  train loss: {:.10f}".format(train_loss.item()) \
-              + "  val acc: {:.10f}".format(val_acc))
+              + "  train loss: {:.4f}".format(train_loss.item()) \
+              + "  test acc: {:.4f}".format(test_acc))
 
-    z = model.campute()
-    test_acc = calculate_acc(tlx.gather(z, data['train_idx']), tlx.gather(graph.y, data['train_idx']),
-                             tlx.gather(z, data['test_idx']), tlx.gather(graph.y, data['test_idx']),
-                             max_iter=300)
+    z = z_test
+    train_x, test_x, train_y, test_y = model_selection.train_test_split(z, tlx.convert_to_numpy(graph.y),
+                                                                        test_size=0.2, shuffle=True)
+    test_acc = calculate_acc(train_x, train_y, test_x, test_y)
     print("Test acc:  {:.4f}".format(test_acc))
     return test_acc
 
@@ -84,11 +79,11 @@ if __name__ == '__main__':
     parser.add_argument('--dataset', type=str, default='cora', help='dataset')
     parser.add_argument("--dataset_path", type=str, default=r'../', help="path to save dataset")
     parser.add_argument("--best_model_path", type=str, default=r'./', help="path to save best model")
-    parser.add_argument("--lr", type=float, default=0.05, help="learning rate")
-    parser.add_argument("--n_epoch", type=int, default=100, help="number of epoch")
-    parser.add_argument("--embedding_dim", type=int, default=80)
-    parser.add_argument("--lamda", type=float, default=0.5)
-    parser.add_argument("--svdft", type=int, default=200)
+    parser.add_argument("--lr", type=float, default=0.1, help="learning rate")  # 0.3
+    parser.add_argument("--n_epoch", type=int, default=50, help="number of epoch")  # 100
+    parser.add_argument("--embedding_dim", type=int, default=500)  # 80 100 200 300 400
+    parser.add_argument("--lamda", type=float, default=0.5)  # 0.5
+    parser.add_argument("--svdft", type=int, default=300)  # 300
 
     args = parser.parse_args()
 
--- test_gammagl/gammagl/models/tadw.py
+++ test_gammagl/gammagl/models/tadw.py
@@ -6,44 +6,10 @@ from gammagl.utils import add_self_loops
 from gammagl.utils import degree
 from scipy.sparse.linalg import svds
 
-EPS = 1e-15
+lower_control = 10 ** (-15)
 
 
 class TADWModel(tlx.nn.Module):
-    r"""The Node2Vec model from the
-    `"node2vec: Scalable Feature Learning for Networks"
-    <https://arxiv.org/abs/1607.00653>`_ paper where random walks of
-    length :obj:`walk_length` are sampled in a given graph, and node embeddings
-    are learned via negative sampling optimization.
-
-    Parameters
-    ----------
-        edge_index: Iterable
-            The edge indices.
-        edge_weight: Iterable
-            The edge weight.
-        embedding_dim: int
-            The size of each embedding vector.
-        walk_length: int
-            The walk length.
-        p: float
-            Likelihood of immediately revisiting a node in the walk.
-        q: float
-            Control parameter to interpolate between breadth-first strategy and depth-first strategy.
-        num_walks: int
-            The number of walks to sample for each node.
-        window_size: int
-            The actual context size which is considered for
-            positive samples. This parameter increases the effective sampling
-            rate by reusing samples across different source nodes.
-        num_negatives: int
-            The number of negative samples to use for each positive sample.
-        num_nodes: int
-            The number of nodes.
-        name: str
-            model name
-    """
-
     def __init__(
             self,
             edge_index,
@@ -79,8 +45,9 @@ class TADWModel(tlx.nn.Module):
         src = edge_index[0]
         degs = degree(src, num_nodes=num_nodes, dtype=tlx.float32)
         norm_degs = tlx.convert_to_numpy(1.0 / degs)
-        # 初始化A
+
         A = [[0] * num_nodes for _ in range(num_nodes)]
+
         length = edge_index.shape[1]
         for i in range(length):
             src = edge_index[0][i]
@@ -102,27 +69,24 @@ class TADWModel(tlx.nn.Module):
                 IDF = tlx.convert_to_numpy(tlx.log(tlx.convert_to_tensor(num_nodes / temp)))
                 feature[:, i] = feature[:, i] * IDF
 
-        # SVD
         U, S, V = svds(feature, k=self.svdft)
         text_feature = U.dot(np.diag(S))
 
-        # length = len(text_feature)
-        # for i in range(length):
-        #     text_feature[i] = text_feature[i] / np.linalg.norm(text_feature[i], ord=2)
+        length = len(text_feature)
+        for i in range(length):
+            text_feature[i] = text_feature[i] / np.linalg.norm(text_feature[i], ord=2)
         # text_feature = text_feature * 0.1
 
         return text_feature
-
-    def forward(self, edge_index):
-        return self.loss()
 
     def fit(self):
         """
         Gradient descent updates for a given number of iterations.
         """
+        loss = self.loss()
         self.update_W()
-        # self.update_H()
-        return self.loss()
+        self.update_H()
+        return loss
 
     def update_W(self):
         """
@@ -130,15 +94,10 @@ class TADWModel(tlx.nn.Module):
         """
         H_T = np.dot(self.H, self.T)  # k*|V|
         grad = self.lamda * self.W - np.dot(H_T, self.M - np.dot(np.transpose(H_T), self.W))
+        # grad = self.lamda * self.W - 2 / np.prod(self.M.shape) * np.dot(H_T, self.M - np.dot(H_T.T, self.W))
         self.W = self.W - self.lr * grad
         # Overflow control
-        self.W[self.W < 10 ** (-15)] = 10 ** (-15)
-
-        # inside = self.M - np.dot(np.dot(np.transpose(self.W), self.H), self.T)  # |V|*|V|
-        # grad = self.lamda * self.W - 2 * np.dot(H_T, inside)  # K*ft
-        # self.W = self.W - self.lr * grad
-        # # Overflow control
-        # self.W[self.W < 10 ** (-15)] = 10 ** (-15)
+        self.W[self.W < lower_control] = lower_control
 
     def update_H(self):
         """
@@ -146,24 +105,25 @@ class TADWModel(tlx.nn.Module):
         """
         inside = self.M - np.dot(np.dot(np.transpose(self.W), self.H), self.T)  # |V|*|V|
         grad = self.lamda * self.H - np.dot(np.dot(self.W, inside), np.transpose(self.T))  # K*ft
+        # grad = self.lamda * self.H - 2 / np.prod(self.M.shape) * np.dot(np.dot(self.W, inside), self.T.T)
         self.H = self.H - self.lr * grad
         # Overflow control
-        self.H[self.H < 10 ** (-15)] = 10 ** (-15)
-
-        # inside = self.M - np.dot(np.dot(np.transpose(self.W), self.H), self.T)  # |V|*|V|
-        # grad = self.lamda * self.H - 2 * np.dot(self.W, inside)  # K*ft
-        # self.H = self.H - self.lr * grad
-        # # Overflow control
-        # self.H[self.H < 10 ** (-15)] = 10 ** (-15)
+        self.H[self.H < lower_control] = lower_control
 
     def loss(self):
+
+        # self.score_matrix = self.M - np.dot(np.dot(np.transpose(self.W), self.H), self.T)
+        # main_loss = np.sum(np.square(self.score_matrix))
+
         self.score_matrix = self.M - np.dot(np.dot(np.transpose(self.W), self.H), self.T)
+        # main_loss = np.mean(np.square(self.score_matrix))
+        # regul_1 = self.lamda * np.mean(np.square(self.W)) / 2
+        # regul_2 = self.lamda * np.mean(np.square(self.H)) / 2
         main_loss = np.sum(np.square(self.score_matrix))
+        regul_1 = self.lamda * np.sum(np.square(self.W)) / 2
+        regul_2 = self.lamda * np.sum(np.square(self.H)) / 2
+        main_loss = main_loss + regul_1 + regul_2
 
-        # regul_1 = self.lamda * np.sum(np.square(self.W))
-        # regul_2 = self.lamda * np.sum(np.square(self.H))
-
-        # print(iteration, main_loss, regul_1, regul_2)
         return main_loss
 
     def campute(self):
